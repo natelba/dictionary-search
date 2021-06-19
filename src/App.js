88facecaed608a65525fed65656ebe7bfb3fa1e3
natelba <77124289+natelba@users.noreply.github.com>
2021-06-19 11:33:07 -0400

    adding search engine

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
         Hello
       </header>
+      <main>
+      <Dictionary />
+      </main>
+      <footer className="App-footer">
+        <small>Coded by Natalia Guardia and is <a href="https://github.com/natelba/dictionary-app">Open-Sourced</a></small>
+      </footer>
+      </div>
     </div>
   );  
 }
