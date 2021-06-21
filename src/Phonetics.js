3a77732ebc62aa6220be88ecd7852e4b47682c2a
natelba <77124289+natelba@users.noreply.github.com>
2021-06-21 14:46:23 -0400

    adding phonetics comp

---
+++ src/Phonetics.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Phonetics (props) {
+    console.log(props.phonetic);
+    return (
+        <div className="Phonetics">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+                Listen
+            </a>
+            <br />
+            {props.phonetic.text}
+        </div>
+    )
+}
+
+// try to get api react audio button
--- src/Results.js
+++ src/Results.js
@@ -1,17 +1,26 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
+
 
 export default function Results(props){
     if (props.results){
        return <div className="Results">
            <h2>{props.results.word}</h2>
-           {props.results.meanings.map(function(meaning, index) {
-               return (
-               <div key={index}>
-                   <Meaning meaning={meaning} />
-               </div>
-               )
-           })}
+            {props.results.phonetics.map(function(phonetic, index) {
+                return (
+                    <div key={index}>
+                        <Phonetics phonetic={phonetic} />
+                    </div>
+                );
+            })}
+            {props.results.meanings.map(function(meaning, index) {
+                return (
+                    <div key={index}>
+                        <Meaning meaning={meaning} />
+                    </div>
+               );
+            })}
        </div> 
     } else {
         return null;
